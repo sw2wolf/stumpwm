63c362e8eb278207906efb6ca5268bacabaacd60
sw2wolf <czsq888@163.com>
2013-08-07 13:38:46 +0800

    adding scripts in ./bin to create stumpwm directly.

---
+++ bin/make-image-clisp.lisp
@@ -0,0 +1,23 @@
+#!/bin/sh
+#|
+exec clisp -q -q -modern -ansi -norc $0 ${1+"$@"}
+exit
+|#
+
+(load "~/quicklisp/asdf")
+
+(push #p"~/quicklisp/dists/quicklisp/software/cl-ppcre-2.0.4/" asdf:*central-registry*)
+(asdf:oos 'asdf:load-op :cl-ppcre)
+
+(push #p"~/quicklisp/dists/quicklisp/software/clx-20121125-git/" asdf:*central-registry*)
+(asdf:oos 'asdf:load-op :clx)
+
+(push #p"~/quicklisp/local-projects/stumpwm/" asdf:*central-registry*)
+(asdf:oos 'asdf:load-op :stumpwm)
+
+(ext:saveinitmem "stumpwm" :init-function (lambda ()
+                                            (stumpwm:stumpwm)
+                                            (ext:quit))
+     :executable t :keep-global-handlers t :quiet t :norc t)
+
+(quit)
