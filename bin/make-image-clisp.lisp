1bf5a6c3bb36ca3889e6fa921b55f7e9a426e4a6
sw2wolf <czsq888@163.com>
2014-02-15 16:06:27 +0800

    running (ql:update-dist "quicklisp")

--- bin/make-image-clisp.lisp
+++ bin/make-image-clisp.lisp
@@ -5,12 +5,11 @@ exit
 |#
 
 (load "~/quicklisp/asdf")
-;(require :asdf)
 
-(push #p"~/quicklisp/dists/quicklisp/software/cl-ppcre-2.0.4/" asdf:*central-registry*)
+(push #p"~/quicklisp/dists/quicklisp/software/cl-ppcre-2.0.7/" asdf:*central-registry*)
 (asdf:oos 'asdf:load-op :cl-ppcre)
 
-(push #p"~/quicklisp/dists/quicklisp/software/clx-20121125-git/" asdf:*central-registry*)
+(push #p"~/quicklisp/dists/quicklisp/software/clx-20131003-git/" asdf:*central-registry*)
 (asdf:oos 'asdf:load-op :clx)
 
 (push #p"~/quicklisp/local-projects/stumpwm/" asdf:*central-registry*)
