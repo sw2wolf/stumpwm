06fb1305efae6de0aeeac792b80931c2ff48cf03
sw2wolf <sw2wolf@ob56.zsoft.com>
2015-01-24 16:31:57 +0800

    misc.

--- bin/make-image-clisp.lisp
+++ bin/make-image-clisp.lisp
@@ -6,10 +6,10 @@ exit
 
 (load "~/quicklisp/asdf")
 
-(push #p"~/quicklisp/dists/quicklisp/software/cl-ppcre-2.0.7/" asdf:*central-registry*)
+(push #p"~/quicklisp/dists/quicklisp/software/cl-ppcre-2.0.9/" asdf:*central-registry*)
 (asdf:oos 'asdf:load-op :cl-ppcre)
 
-(push #p"~/quicklisp/dists/quicklisp/software/clx-20131003-git/" asdf:*central-registry*)
+(push #p"~/quicklisp/dists/quicklisp/software/clx-20150113-git/" asdf:*central-registry*)
 (asdf:oos 'asdf:load-op :clx)
 
 (push #p"~/quicklisp/local-projects/stumpwm/" asdf:*central-registry*)
